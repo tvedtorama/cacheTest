de4da7d8c6bce0fc88e760371384f54cbab93e49
Jørgen Tvedt <jorgen.tvedt@knowit.no>
2016-03-05 09:05:18 +0100

    Added loading of view using ejs

--- server/main.js
+++ server/main.js
@@ -1,3 +1,22 @@
+import express from 'Express'
+import http from 'http'
+
+
+var port = process.env.PORT || 3000;
+
+
+var app = express();
+var server = http.createServer(app);
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build/web'));
+
+app.use('/main', (req, res) => {
+				res.render('main.ejs', {id: 'testId'})
+			})
+
+app.listen(port, () => console.log('connected: '));
 
 let start = () =>
 {
