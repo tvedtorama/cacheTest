7361b93ff1741ab3691ddb6f5d331332384bfb52
Jørgen Tvedt <jorgen.tvedt@knowit.no>
2016-03-06 07:45:06 +0100

    Added working watch logic for all steps, including watchify for browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,31 @@ var babelify = require('babelify');
 
 
 function compileBrowserify(file, folder, watch) {
-	var bundler = watchify(browserify(file, { debug: true }).transform(babelify.configure({
-        presets: ['react', 'es2015', 'stage-0']})))
+	var bundlerRaw = browserify(file, { debug: true })
+	var bundler = watch ? watchify(bundlerRaw) : bundlerRaw
+	bundler.transform(babelify.configure({
+        presets: ['react', 'es2015', 'stage-0']}))
 
-	return bundler.bundle()
-	  .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source(file))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(folder));
+
+	var rebundle = function() {
+		return bundler
+			.bundle()
+			.on('error', function(err) { console.error(err); this.emit('end'); })
+			.on('end', function() { console.log('Done browserifying'); })
+			.pipe(source(file))
+			.pipe(buffer())
+			.pipe(sourcemaps.init({ loadMaps: true }))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest(folder));
+	}
+	bundler.on('update', rebundle)
+	return rebundle()
 }
 
 function compile(fileSpec, output, folder) {
-	return gulp.src(fileSpec)
+	return gulp
+		.src(fileSpec)
+		.on('end', function() { console.log('Done compiling'); })
 		.pipe(sourcemaps.init())
 		.pipe(gulpBabel({presets: ['react', 'es2015', 'stage-0']}))
 		.pipe(concat(output))
@@ -33,16 +44,13 @@ function compile(fileSpec, output, folder) {
 }
 
 function compileRuntime(watch) {
-  return compileBrowserify("./app/main.js", 'build/web');
+  return compileBrowserify("./app/main.js", 'build/web', watch);
 }
 
-function compileServer(watch) {
-	return compile('./server/main.js', 'main.js', './build/server') 
-}
+var serverPath = './server/main.js'
 
-function watch() {
-  compileRuntime(true);
-  // compileServer(true);
+function compileServer() {
+	return compile(serverPath, 'main.js', './build/server') 
 }
 
 function watchForTests() {
@@ -53,22 +61,31 @@ function sassIt(path) {
   console.log('sassing...')
   return gulp.src(path).
     pipe(sass().on('error', sass.logError)).
-    pipe(gulp.dest('./build/css/'));
+    pipe(gulp.dest('./build/web/css/'));
 }
 
 gulp.task('buildServer', function() { return compileServer(); });
 gulp.task('buildRuntime', function() { return compileRuntime(); });
-gulp.task('watch', function() { return watch(); });
+gulp.task('watchServer', function() { return gulp.watch(serverPath, function() {compileServer()}) });
+gulp.task('watchRuntime', function() { return compileRuntime(true); });
 gulp.task('watchForTests', function() { return watchForTests(); });
 
+var sassPath = 'sass/**/*.scss'
+
 gulp.task('styles', function() {  
-  var path = 'sass/**/*.scss'
-  sassIt(path)
-  gulp.watch(path, function () {
-    sassIt(path)
-  });
+	return sassIt(sassPath)
+	gulp.watch(sassPath, function () {
+	 sassIt(sassPath)
+	});
 });
 
-gulp.task('build', ['buildRuntime', 'buildServer'])
+gulp.task('watchStyles', function() {  
+	gulp.watch(sassPath, function () {
+		sassIt(sassPath)
+	});
+});
+
+gulp.task('build', ['buildRuntime', 'buildServer', 'styles'])
+gulp.task('watch', ['styles', 'watchStyles', 'buildServer', 'watchServer', 'watchRuntime']);
 
 gulp.task('default', ['watch', 'styles']);
